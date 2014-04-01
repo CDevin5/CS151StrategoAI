905f4f35777ac757f236274c0663e0fcd2379630
May Lynn Forssen <mforssen@hmc.edu>
2014-03-31 21:28:03 -0700

    fixed some bugs in state

--- state.py
+++ state.py
@@ -2,7 +2,7 @@ from piece import *
 
 class GameState:
 
-	def __init__(self, agent0setup, agent1setup, agent0dead = [], agent1dead = [], layout):
+	def __init__(self, layout, agent0setup, agent1setup, agent0dead = [], agent1dead = []):
 		self.layout = layout
 
 		self.player0pieces_alive = agent0setup
@@ -16,24 +16,24 @@ class GameState:
 		self.pieces = Grid(width, height)
 
 		for x in range(width):
-            for y in range(height):
-                walls = self.layout.walls
-                self.state[x][y] = self._wallStr(walls[x][y])
+			for y in range(height):
+				walls = self.layout.walls
+				self.state[x][y] = self._wallStr(walls[x][y])
 				self.pieces[x][y] = None
 
-        for piece in self.player0pieces_alive:
-            self.state[x][y] = piece.rank
-            self.pieces[x][y] = piece
+		for piece in self.player0pieces_alive:
+			self.state[x][y] = piece.rank
+			self.pieces[x][y] = piece
 
-        for piece in self.player1pieces_alive:
-            self.state[x][y] = piece.rank
-            self.pieces[x][y] = piece
+		for piece in self.player1pieces_alive:
+			self.state[x][y] = piece.rank
+			self.pieces[x][y] = piece
 
-    def _wallStr( self, hasWall ):
-        if hasWall:
-            return '%'
-        else:
-            return ' '
+	def _wallStr( self, hasWall ):
+		if hasWall:
+			return '%'
+		else:
+			return ' '
 
 	def __str__(self):
 		return str(self.grid)
@@ -104,7 +104,7 @@ class GameState:
 			neighbors.append((x,y-1), SOUTH)
 
 	def copy(self):
-		copyState = GameState(self.player0pieces_alive, self.player1pieces_alive, self.player0pieces_dead, self.player1pieces_dead, self.layout)
+		copyState = GameState(self.layout, self.player0pieces_alive, self.player1pieces_alive, self.player0pieces_dead, self.player1pieces_dead)
 
 	def getState(self, agent):
 		return self.state
@@ -169,7 +169,7 @@ class GameState:
 						actions.append(piece, direction, pos)
 			
 	def isWon(agent):
-		if agent == 0
+		if agent == 0:
 			if getFlag(1) == None:
 				return True
 			else:
@@ -183,122 +183,122 @@ class GameState:
 
 
 class Directions:
-    NORTH = 'North'
-    SOUTH = 'South'
-    EAST  = 'East'
-    WEST  = 'West'
+	NORTH = 'North'
+	SOUTH = 'South'
+	EAST  = 'East'
+	WEST  = 'West'
 
 class Grid:
-    """
-    A 2-dimensional array of objects backed by a list of lists.  Data is accessed
-    via grid[x][y] where (x,y) are positions on a Pacman map with x horizontal,
-    y vertical and the origin (0,0) in the bottom left corner.
+	"""
+	A 2-dimensional array of objects backed by a list of lists.  Data is accessed
+	via grid[x][y] where (x,y) are positions on a Pacman map with x horizontal,
+	y vertical and the origin (0,0) in the bottom left corner.
 
-    The __str__ method constructs an output that is oriented like a pacman board.
-    """
-    def __init__(self, width, height, initialValue=False, bitRepresentation=None):
-        if initialValue not in [False, True]: raise Exception('Grids can only contain booleans')
-        self.CELLS_PER_INT = 30
+	The __str__ method constructs an output that is oriented like a pacman board.
+	"""
+	def __init__(self, width, height, initialValue=False, bitRepresentation=None):
+		if initialValue not in [False, True]: raise Exception('Grids can only contain booleans')
+		self.CELLS_PER_INT = 30
 
-        self.width = width
-        self.height = height
-        self.data = [[initialValue for y in range(height)] for x in range(width)]
-        if bitRepresentation:
-            self._unpackBits(bitRepresentation)
+		self.width = width
+		self.height = height
+		self.data = [[initialValue for y in range(height)] for x in range(width)]
+		if bitRepresentation:
+			self._unpackBits(bitRepresentation)
 
-    def __getitem__(self, i):
-        return self.data[i]
+	def __getitem__(self, i):
+		return self.data[i]
 
-    def __setitem__(self, key, item):
-        self.data[key] = item
+	def __setitem__(self, key, item):
+		self.data[key] = item
 
-    def __str__(self):
-        out = [[str(self.data[x][y])[0] for x in range(self.width)] for y in range(self.height)]
-        out.reverse()
-        return '\n'.join([''.join(x) for x in out])
+	def __str__(self):
+		out = [[str(self.data[x][y])[0] for x in range(self.width)] for y in range(self.height)]
+		out.reverse()
+		return '\n'.join([''.join(x) for x in out])
 
-    def __eq__(self, other):
-        if other == None: return False
-        return self.data == other.data
+	def __eq__(self, other):
+		if other == None: return False
+		return self.data == other.data
 
-    def __hash__(self):
-        # return hash(str(self))
-        base = 1
-        h = 0
-        for l in self.data:
-            for i in l:
-                if i:
-                    h += base
-                base *= 2
-        return hash(h)
+	def __hash__(self):
+		# return hash(str(self))
+		base = 1
+		h = 0
+		for l in self.data:
+			for i in l:
+				if i:
+					h += base
+				base *= 2
+		return hash(h)
 
-    def copy(self):
-        g = Grid(self.width, self.height)
-        g.data = [x[:] for x in self.data]
-        return g
+	def copy(self):
+		g = Grid(self.width, self.height)
+		g.data = [x[:] for x in self.data]
+		return g
 
-    def deepCopy(self):
-        return self.copy()
+	def deepCopy(self):
+		return self.copy()
 
-    def shallowCopy(self):
-        g = Grid(self.width, self.height)
-        g.data = self.data
-        return g
+	def shallowCopy(self):
+		g = Grid(self.width, self.height)
+		g.data = self.data
+		return g
 
-    def count(self, item =True ):
-        return sum([x.count(item) for x in self.data])
+	def count(self, item =True ):
+		return sum([x.count(item) for x in self.data])
 
-    def asList(self, key = True):
-        list = []
-        for x in range(self.width):
-            for y in range(self.height):
-                if self[x][y] == key: list.append( (x,y) )
-        return list
+	def asList(self, key = True):
+		list = []
+		for x in range(self.width):
+			for y in range(self.height):
+				if self[x][y] == key: list.append( (x,y) )
+		return list
 
-    def packBits(self):
-        """
-        Returns an efficient int list representation
+	def packBits(self):
+		"""
+		Returns an efficient int list representation
 
-        (width, height, bitPackedInts...)
-        """
-        bits = [self.width, self.height]
-        currentInt = 0
-        for i in range(self.height * self.width):
-            bit = self.CELLS_PER_INT - (i % self.CELLS_PER_INT) - 1
-            x, y = self._cellIndexToPosition(i)
-            if self[x][y]:
-                currentInt += 2 ** bit
-            if (i + 1) % self.CELLS_PER_INT == 0:
-                bits.append(currentInt)
-                currentInt = 0
-        bits.append(currentInt)
-        return tuple(bits)
+		(width, height, bitPackedInts...)
+		"""
+		bits = [self.width, self.height]
+		currentInt = 0
+		for i in range(self.height * self.width):
+			bit = self.CELLS_PER_INT - (i % self.CELLS_PER_INT) - 1
+			x, y = self._cellIndexToPosition(i)
+			if self[x][y]:
+				currentInt += 2 ** bit
+			if (i + 1) % self.CELLS_PER_INT == 0:
+				bits.append(currentInt)
+				currentInt = 0
+		bits.append(currentInt)
+		return tuple(bits)
 
-    def _cellIndexToPosition(self, index):
-        x = index / self.height
-        y = index % self.height
-        return x, y
+	def _cellIndexToPosition(self, index):
+		x = index / self.height
+		y = index % self.height
+		return x, y
 
-    def _unpackBits(self, bits):
-        """
-        Fills in data from a bit-level representation
-        """
-        cell = 0
-        for packed in bits:
-            for bit in self._unpackInt(packed, self.CELLS_PER_INT):
-                if cell == self.width * self.height: break
-                x, y = self._cellIndexToPosition(cell)
-                self[x][y] = bit
-                cell += 1
+	def _unpackBits(self, bits):
+		"""
+		Fills in data from a bit-level representation
+		"""
+		cell = 0
+		for packed in bits:
+			for bit in self._unpackInt(packed, self.CELLS_PER_INT):
+				if cell == self.width * self.height: break
+				x, y = self._cellIndexToPosition(cell)
+				self[x][y] = bit
+				cell += 1
 
-    def _unpackInt(self, packed, size):
-        bools = []
-        if packed < 0: raise ValueError, "must be a positive integer"
-        for i in range(size):
-            n = 2 ** (self.CELLS_PER_INT - i - 1)
-            if packed >= n:
-                bools.append(True)
-                packed -= n
-            else:
-                bools.append(False)
-        return bools
+	def _unpackInt(self, packed, size):
+		bools = []
+		if packed < 0: raise ValueError, "must be a positive integer"
+		for i in range(size):
+			n = 2 ** (self.CELLS_PER_INT - i - 1)
+			if packed >= n:
+				bools.append(True)
+				packed -= n
+			else:
+				bools.append(False)
+		return bools
