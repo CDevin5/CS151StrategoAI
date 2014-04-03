08a5e5b9a530093127a2c140f1912eb5851a35d3
Michelle Chesley <mmchesley@gmail.com>
2014-04-02 19:35:40 -0700

    Kind of fixed some things

--- Agent.py
+++ Agent.py
@@ -1,5 +1,6 @@
 import random
 import state
+from piece import *
 
 class Agent:
     """
@@ -23,16 +24,30 @@ class RandomAgent(Agent):
     An agent that picks a random action.
     """
     def getAction(self, state):
-        actions = state.getLegalActions()
+        actions = state.getLegalActions(self.index)
         return random.choice(actions)
     def makeSetup(self):
         """ Returns a list of pieces"""
         startingRanks = [FLAG, SPY, SCOUT, SCOUT, MINER, MINER, GENERAL, MARSHALL, BOMB, BOMB]
         startingSpots = random.sample(self.getStartSpots(), len(startingRanks))
         pieces = []
-        for i in len(startingRanks):
+        for i in range(len(startingRanks)):
             pieces += [Piece(startingRanks[i], startingSpots[i], self.index)]
         return pieces
+
+    def getStartSpots(self):
+        """ Generates a list of the positions available for initial setup """
+        spots = []
+        if self.index == 0:
+            startRow = 0
+            endRow = 3
+        if self.index == 1:
+            startRow = 5
+            endRow = 8
+        for row in range(startRow, endRow):
+            for col in range(8):
+                spots += [(col, row)]
+        return spots
 
 class HumanAgent(Agent):
     """
@@ -41,18 +56,25 @@ class HumanAgent(Agent):
     def getAction(self, state):
         piece = None
         newPos = None
+        actions = state.getLegalActions(self.index)
+        print "Legal actions:", [(str(p), p.position, pos) for (p, pos) in actions]
         while piece == None:
             print state
             userInput = raw_input("What is your move? (x0,y0) (x1,y1) ").split()
             oldList = list(userInput[0])
-            oldPos = (oldList[1], oldList[3])
+            oldPos = (int(oldList[1]), int(oldList[3]))
             newList = list(userInput[1])
-            newPos = (newList[1], newList[3])
-            piece = state.getPieceAtPos(self, oldPos)
+            newPos = (int(newList[1]), int(newList[3]))
+            piece = state.getPieceAtPos(oldPos)
+            print "Old pos", oldPos
+            print "New pos", newPos
             if piece == None:
                 print "Invalid initial position"
+            elif (piece, newPos) not in actions:
+                print "Not a legal action"
+                piece = None
             else:
-                print "moving", piece, "from", oldPos, "to", pos
+                print "moving", piece, "from", oldPos, "to", newPos
         return (piece, newPos)
 
     def makeSetup(self):
@@ -60,7 +82,22 @@ class HumanAgent(Agent):
         startingRanks = [FLAG, SPY, SCOUT, SCOUT, MINER, MINER, GENERAL, MARSHALL, BOMB, BOMB]
         startingSpots = random.sample(self.getStartSpots(), len(startingRanks))
         pieces = []
-        for i in len(startingRanks):
+        for i in range(len(startingRanks)):
             pieces += [Piece(startingRanks[i], startingSpots[i], self.index)]
+        print [(str(p), p.position) for p in pieces]
         return pieces
+
+    def getStartSpots(self):
+        """ Generates a list of the positions available for initial setup """
+        spots = []
+        if self.index == 0:
+            startRow = 1
+            endRow = 4
+        if self.index == 1:
+            startRow = 6
+            endRow = 9
+        for row in range(startRow, endRow):
+            for col in range(1,9):
+                spots += [(row, col)]
+        return spots
 
